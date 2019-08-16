ebfb6b2acb93629ee5263e18f4f506f67c244c83
OlegBraginskyy <obr@sitecore.net>
2019-08-16 16:07:23 +0300

    Add CheckModifiedParameters for SelectMapPoint

---
+++ src/Sitecore.Support.351468/SelectMapPoint.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Web.UI.Sheer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.XA.Feature.Maps.Commands
+{
+    public class SelectMapPoint : Sitecore.XA.Feature.Maps.Commands.SelectMapPoint
+    {
+        protected override void Run(ClientPipelineArgs args)
+        {
+            CheckModifiedParameters parameters = new CheckModifiedParameters();
+            parameters.ResumePreviousPipeline = true;
+            if (SheerResponse.CheckModified(parameters))
+            {
+                base.Run(args);
+            }
+        }
+    }
+}
