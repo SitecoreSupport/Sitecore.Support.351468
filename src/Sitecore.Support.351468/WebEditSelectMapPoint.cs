212c134e71702ca3efb943c12cc34b3c8b154f96
OlegBraginskyy <obr@sitecore.net>
2019-08-16 16:08:01 +0300

    Add CheckModifiedParameters for WebEditSelectMapPoint

---
+++ src/Sitecore.Support.351468/WebEditSelectMapPoint.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Web.UI.Sheer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.XA.Feature.Maps.Commands
+{
+    public class WebEditSelectMapPoint : Sitecore.XA.Feature.Maps.Commands.WebEditSelectMapPoint
+    {
+        protected override void Run(ClientPipelineArgs args)
+        {
+            CheckModifiedParameters parameters = new CheckModifiedParameters();
+            parameters.ResumePreviousPipeline = true;
+            if (SheerResponse.CheckModified(parameters))
+            {
+                base.Run(args);
+            }
+        }
+    }
+}
